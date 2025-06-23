1f946a63e91221c3b7f2dd0a2fd29b6d802fdead
Masensen <masensen.dev@proton.me>
2025-06-23 11:59:57 -0400

    fix

--- bin/Formating_root.sh
+++ bin/Formating_root.sh
@@ -12,39 +12,33 @@ btrfs subvolume create /mnt/@log          # /var/log - logs
 btrfs subvolume create /mnt/@cache        # /var/cache - caches
 btrfs subvolume create /mnt/@pkg          # /var/lib/pacman - package DB & cache
 btrfs subvolume create /mnt/@tmp          # /var/tmp - temp files
+
+btrfs subvolume list /mnt
+
+sleep 10
+
 #
 umount /mnt
 #
 mount -o subvol=@,rw,noatime,autodefrag,ssd,compress=zstd /dev/mapper/vg0-root /mnt
 #
-mkdir -p /mnt/home
-mkdir -p /mnt/.snapshots
-mkdir -p /mnt/vms
-mkdir -p /mnt/var
-mkdir -p /mnt/var/log
-mkdir -p /mnt/var/cache
-mkdir -p /mnt/var/lib/pacman
-mkdir -p /mnt/tmp
 
-#
+mkdir -p /mnt/{home,.snapshots,vms,var,tmp,boot}
+
 mount -o subvol=@home,rw,noatime,autodefrag,ssd,compress=zstd /dev/mapper/vg0-root /mnt/home
-
 mount -o subvol=@snapshots,rw,noatime,autodefrag,ssd,compress=zstd /dev/mapper/vg0-root /mnt/.snapshots
-
-mount -o subvol=@vms,noatime, nodatacow,ssd /dev/mapper/vg0-root /mnt/vms
+mount -o subvol=@tmp,rw,noatime,autodefrag,ssd,compress=zstd /dev/mapper/vg0-root /mnt/tmp
+mount -o subvol=@vms,noatime,nodatacow,ssd /dev/mapper/vg0-root /mnt/vms
 
 mount -o subvol=@var,rw,noatime,autodefrag,ssd,compress=zstd /dev/mapper/vg0-root /mnt/var
 
+mkdir -p /mnt/var/{log,cache,lib/pacman}
+
 mount -o subvol=@log,rw,noatime,autodefrag,ssd,compress=zstd /dev/mapper/vg0-root /mnt/var/log
-
 mount -o subvol=@cache,rw,noatime,autodefrag,ssd,compress=zstd /dev/mapper/vg0-root /mnt/var/cache
-
 mount -o subvol=@pkg,rw,noatime,autodefrag,ssd,compress=zstd /dev/mapper/vg0-root /mnt/var/lib/pacman
 
-mount -o subvol=@tmp,rw,noatime,autodefrag,ssd,compress=zstd /dev/mapper/vg0-root /mnt/tmp
 #
-mkdir -p mnt/boot
-mount /dev/${DISK}1 mnt/boot
-
+mount ${DISK}1 /mnt/boot
 
 sleep 10
